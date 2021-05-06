69de485ec2dfc2fbe4c42dc051aa15ade579d3f4
Stephen Chan <stephen.s.c.chan@gmail.com>
2021-05-05 19:07:28 -0700

    Add skeleton UI

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
 import './App.css';
+
+function SiteWeatherOutputs(params) {
+  return (
+    <table>
+
+      <tbody>
+        <tr>
+          <td>
+            Temperature Results
+        with Max Temperature: <input id="maxTemperatureInput"></input><button>Load</button>
+          </td>
+        </tr>
+        <SiteWeatherOutputEntry name="Adelaide" maxTemperature="55"/>
+        <SiteWeatherOutputEntry name="Melbourne" maxTemperature="45"/>
+      </tbody>
+    </table>
+  )
+}
+function SiteWeatherOutputEntry(params) {
+  return (
+    <tr><td>{params.name}</td><td>{params.maxTemperature} degrees</td></tr>
+  );
+}
+
+function SiteManagement(params) {
+  return (
+    <div>
+      <div>
+        Add / Remove Site
+    </div>
+      <div>
+      Name: <input id="siteNameInput"></input><br />
+      Latitude: <input id="siteLatitudeInput"></input> <br />
+      Longitude: <input id="siteLongitudeInput"></input> <br />
+      </div>
+      <div>
+      <button>Add</button><button>Remove</button>
+      </div>
+    </div>
+  )
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <SiteManagement />
+
+      <div>
+        <SiteWeatherOutputs />
+      </div>
+
     </div>
   );
 }
