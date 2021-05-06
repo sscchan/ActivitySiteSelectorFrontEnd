d6ffd923f701e7d8eca61cdcd90c304f042b3b99
Stephen Chan <stephen.s.c.chan@gmail.com>
2021-05-06 07:47:54 -0700

    Implement UI in React-Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,57 +1,111 @@
-import './App.css';
+
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Table from 'react-bootstrap/Table';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form';
 
 function SiteWeatherOutputs(params) {
   return (
-    <table>
+    <Container>
+      <Row>
+        <h2>Temperature Results</h2>
+      </Row>
 
-      <tbody>
-        <tr>
-          <td>
-            Temperature Results
-        with Max Temperature: <input id="maxTemperatureInput"></input><button>Load</button>
-          </td>
-        </tr>
-        <SiteWeatherOutputEntry name="Adelaide" maxTemperature="55"/>
-        <SiteWeatherOutputEntry name="Melbourne" maxTemperature="45"/>
-      </tbody>
-    </table>
+      <Row>
+        <Table>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Latitude</th>
+              <th>Longitude</th>
+              <th>Max Temperature</th>
+            </tr>
+          </thead>
+          <tbody>
+            <SiteWeatherOutputEntry name="Adelaide" latitude="-32" longitude="55" maxTemperature="55" />
+            <SiteWeatherOutputEntry name="Melbourne" latitude="-32" longitude="55" maxTemperature="45" />
+            <SiteWeatherOutputEntry name="Sydney" latitude="-32" longitude="55" maxTemperature="45" />
+          </tbody>
+        </Table>
+      </Row>
+    </Container>
+  )
+}
+
+function WeatherFilterParameters(params) {
+  return (
+    <Container>
+      <Row>
+        <h3>Weather Filter Parameters</h3>
+      </Row>
+      <Row>
+        <Col>
+          <Form.Group>
+            <Form.Label>Max Temperature</Form.Label>
+            <Form.Control placeholder="None" id="maxTemperatureInput" />
+          </Form.Group>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <Button>Load Results</Button>
+        </Col>
+      </Row>
+    </Container>
   )
 }
 function SiteWeatherOutputEntry(params) {
   return (
-    <tr><td>{params.name}</td><td>{params.maxTemperature} degrees</td></tr>
+    <tr>
+      <td>{params.name}</td>
+      <td>{params.latitude}</td>
+      <td>{params.longitude}</td>
+      <td>{params.maxTemperature} degrees</td>
+    </tr>
   );
 }
 
 function SiteManagement(params) {
   return (
-    <div>
-      <div>
-        Add / Remove Site
-    </div>
-      <div>
-      Name: <input id="siteNameInput"></input><br />
-      Latitude: <input id="siteLatitudeInput"></input> <br />
-      Longitude: <input id="siteLongitudeInput"></input> <br />
-      </div>
-      <div>
-      <button>Add</button><button>Remove</button>
-      </div>
-    </div>
+    <Container>
+      <Row>
+        <h3>Add / Remove Site</h3>
+      </Row>
+
+      <Form>
+        <Row>
+          <Col>
+            <Form.Control placeholder="Name" id="siteNameInput" />
+          </Col>
+          <Col>
+            <Form.Control placeholder="Latitude" id="siteLatitudeInput" />
+          </Col>
+          <Col>
+            <Form.Control placeholder="Longitude" id="siteLongitudeInput" />
+          </Col>
+          <Col>
+            <Button>Add</Button>&nbsp;
+            <Button>Remove</Button>
+          </Col>
+        </Row>
+      </Form>
+    </Container>
   )
 }
 
 function App() {
   return (
-    <div className="App">
-
+    <Container className="App">
+      <h2>Site Weather</h2>
       <SiteManagement />
+      <br />
+      <WeatherFilterParameters />
+      <br />
+      <SiteWeatherOutputs />
 
-      <div>
-        <SiteWeatherOutputs />
-      </div>
-
-    </div>
+    </Container>
   );
 }
 
